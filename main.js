f26e49978a4f5cb51723f29acb9500645e6b7943
mariconpe <marinapereiraecp@gmail.com>
2022-07-21 15:52:08 -0300

    fix: makes getMovie function able to skip not found pages

--- main.js
+++ main.js
@@ -8,44 +8,47 @@ import {
 const title= document.getElementById('movie-title');
 const overview = document.getElementById('movie-overview');
 const poster = document.getElementById('movie-poster');
+const lang = document.getElementById('movie-language');
+const average = document.getElementById('movie-average');
+const button = document.getElementById('shuffle-button');
+const details = document.getElementById('movie-details');
+var click = 0;
 
-$('#shuffle-button').click(function(){
-  let movieID = String(Math.floor((Math.random()*500000)+55));
-  let APIRequestURL = BASE_URL+movieID+'?'+API_KEY+'&'+language;
-  $.getJSON(APIRequestURL, function(movies){
-    var movieList = [movies.title, movies.overview, movies.poster_path];
-    title.innerText=movieList[0];
-    overview.innerText=movieList[1];
-    poster.setAttribute=('src', IMG_URL + movieList[2]);
-  })
-  .fail(function(){
-    title.innerText='Filme sem título';
-    overview.innerText='Filme sem sumário';
-    poster.setAttribute=('src', './assets/poster-not-available.jpg');
-    poster.setAttribute=('alt', 'Pôster indisponível');
-  });
-});
-      //$("#movie-title").remove();
-      //$("<h2 id='movie-title'>"+movieTitle.title+'</h2>').appendTo('#movie-info');
-      //if (movieTitle.poster_path =! null){
-      //  $("#movie-poster").remove();
-      //  $("<img  id='movie-poster' src='"+IMG_URL + movieTitle.poster_path+"'>").appendTo('figure');
-      //}
-      //else{
-      //  $("#movie-poster").remove();
-      //  $("<img  id='movie-poster' src='./assets/poster-not-available.jpg'>").appendTo('figure');
-      //};
-      //if (movieTitle.overview =! null){
-      //  $("#movie-overview").remove();
-      //  $("<p id='movie-overview'>"+movieTitle.overview+"</p>").appendTo('#movie-info');   
-      //}
-//
-    //})
-    //.fail(function() {             
-    //  $("#movie-title").remove();
-    //  $("<h2 id='movie-title'>"+ 'Filme indisponível'+ "</h2>").appendTo('#movie-info');
-    //  $("#movie-poster").remove();
-    //  $("<img  id='movie-poster' src='./assets/poster-not-available.jpg'>").appendTo('figure');
-    //  $("#movie-overview").remove();
-    //  $("<p id='movie-overview'>"+ 'Sumário indisponível'+ "</p>").appendTo('#movie-info');
+$('#shuffle-button').click(function (){
+    if(click === 0) {
+      poster.setAttribute('style','height:300px');
+      poster.setAttribute('alt', 'Pôster do filme');
+      details.removeAttribute('style', 'display: none');
+      details.setAttribute('style', 'display: flex');      
+    }
+    click++;
+}
+);
 
+function getMovie(){
+  poster.removeAttribute('src');
+  poster.removeAttribute('alt');
+      let movieID = String(Math.floor((Math.random()*500000)+55));
+      let APIRequestURL = BASE_URL+movieID+'?'+API_KEY+'&'+language;
+      console.log(APIRequestURL);
+      $.getJSON(APIRequestURL, function (movies){
+        var movieList = [movies.title, movies.overview, movies.poster_path, movies.vote_average];
+        title.innerText = movieList[0];
+        details.setAttribute('style','display: flex');
+        (movieList[1] != null) && (movieList[1] != "") ? overview.innerText=movieList[1] : overview.innerText = 'Sinopse indisponível';
+        (movieList[2] != null) ? poster.setAttribute('src', IMG_URL + movieList[2]) : poster.setAttribute('src','./assets/poster-not-available.jpg');
+        if ((movieList[3] != null) && (movieList[3] != "")) {
+          average.innerText=movieList[3]+' ★';
+        }  
+        {
+          average.innerText='N/A ★'         
+        };
+        
+      })
+      .fail(
+        getMovie
+      )
+      
+    }
+
+$(button).click(getMovie);
